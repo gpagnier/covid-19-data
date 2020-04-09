095740b6e02cb8d398d51ffcb95c4708019b515e
gpagnier <39566856+gpagnier@users.noreply.github.com>
2020-04-09 17:31:54 -0400

    Create dataViz.R

---
+++ dataViz.R
@@ -0,0 +1,53 @@
+#### Installing packages ####
+install.packages('tidyverse')
+install.packages('brms')
+install.packages('lme4')
+install.packages('optimx')
+install.packages('shiny')
+install.packages('installr')
+install.packages('pracma')
+
+library(tidyverse)
+library(reshape2)
+library(brms)
+library(lme4)
+library(optimx)
+library(shiny)
+library(installr)
+library(pracma)
+
+#Changing some R defaults
+## Base R parameters and loading custom functions ####
+
+options(scipen=999)
+source("C:/Users/Guillaume/Dropbox (Brown)/miscRFunctions/softmax.R")
+d0=read.csv("/Users/Guillaume/Documents/GitHub/covid-19-data/us-counties.csv")
+d0$date<-sub("2020-","",d0$date)
+##Declaring simple county or state visualziation function ####
+#Can plot by county (default) or state
+plotByTime<-function(countyInput,stateInput){
+  #This is for entire states
+  if(missing(countyInput)&& !missing(stateInput)){
+    dTemp<-filter(d0,state==stateInput)
+    dTemp$date<-sub("-","/",dTemp$date)
+    dTemp$date<-sub("0","",dTemp$date)
+    
+    dTemp <- dTemp %>%
+      group_by(date)%>%
+      summarize(deaths=sum(deaths),
+                cases=sum(cases)) %>% 
+      pivot_longer(cols=c(deaths,cases), names_to="type", values_to="vals")
+    dTemp<-dTemp[(nrow(dTemp)/2):nrow(dTemp),]
+    ggplot(dTemp,aes(x=factor(date),y=vals,col=as.factor(type)))+
+      geom_point(
+        size=1.75
+      )+ylim(0,(max(dTemp$vals)+3000))+ylab("")+xlab("Date")
+    #print('oneCond')
+    #THis is there's an error somewhere
+  }else if(missing(countyInput) && missing(stateInput)){
+    print("need to enter 'state' or 'county'")
+    stop()
+  }else{
+    #print('ThreeCond')
+  }
+}
